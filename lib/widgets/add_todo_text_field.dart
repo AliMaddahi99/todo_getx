3501e9bfc81643bb34f9611a870a7f3d8bffc93a
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-02 17:52:29 +0430

    create add_todo_text_field

---
+++ lib/widgets/add_todo_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTodoTextField extends StatelessWidget {
+  const AddTodoTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
