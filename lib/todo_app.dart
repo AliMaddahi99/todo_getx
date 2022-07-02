e8f83a0c91321d278bc0e06890e66a1c80e87904
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-02 16:40:31 +0430

    create home

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_getx/screens/home.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
       title: "TodoAppGetx",
-      home: Container(),
+      home: Home(),
     );
   }
 }
