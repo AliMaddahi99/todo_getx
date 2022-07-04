a60bb1220fbb52cc3c0975c6a3946c7b048492aa
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-04 21:21:43 +0430

    complete edit_todo

--- lib/screens/edit_todo.dart
+++ lib/screens/edit_todo.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_getx/controllers/todo_controller.dart';
+import '../controllers/edit_todo_controller.dart';
 
 class EditTodo extends StatelessWidget {
-  const EditTodo({Key? key}) : super(key: key);
+  EditTodo({Key? key}) : super(key: key);
+
+  final EditTodoController _editTodoController = Get.put(EditTodoController());
+  final TodoController _controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("EditTodo Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade400,
+              blurRadius: 4.0,
+            )
+          ],
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: TextField(
+          decoration: const InputDecoration(
+            hintText: "Edit todo title",
+            border: InputBorder.none,
+          ),
+          controller: _editTodoController.editTodoTextEditingController,
+          focusNode: _editTodoController.editTodoTextFieldFocusNode,
+          onChanged: (text) {
+            var id = _editTodoController.args[0];
+            var edited = _controller.todos[id];
+            edited.title = text;
+            _controller.todos[id] = edited;
+          },
+        ),
+      ),
     );
   }
 }
