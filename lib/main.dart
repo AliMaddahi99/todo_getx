de680a4ac511d39fbc38a5412a2e9a98c3481a0d
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-04 22:29:44 +0430

    initialize GetStorage in main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:todo_getx/todo_app.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const TodoApp());
 }
